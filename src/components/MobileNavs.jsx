57bb49caf4987c6a79b70839bb42e7407311604c
kaushal-33 <varmakaushal33@gmail.com>
2025-07-01 18:52:19 +0530

    added mobile navs

---
+++ src/components/MobileNavs.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Drawer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import ShinyText from "../components/glowingText/ShinyText ";
+import { IoCloseOutline } from "react-icons/io5";
+
+const MobileNavs = ({ isOpen, setIsOpen }) => {
+
+
+    return (
+        <Drawer open={isOpen} onClose={setIsOpen} position="top">
+            <div>
+                <div className="text-white flex justify-between items-center">
+                    <Link className="font-hero capitalize">
+                        <ShinyText text="kaushal varma" speed={3} />
+                    </Link>
+                    <button className="text-xl text-[#7a7a7a] pt-1" onClick={() => setIsOpen(false)}>
+                        <IoCloseOutline />
+                    </button>
+                </div>
+                <ul className="text-[#7a7a7a] capitalize font-semibold nav-link mt-5">
+                    <li className="mb-3">
+                        <Link to={"/"} onClick={() => setIsOpen(false)}>home</Link>
+                    </li>
+                    <li className="mb-3">
+                        <Link onClick={() => setIsOpen(false)}>about</Link>
+                    </li>
+                    <li className="mb-3">
+                        <Link onClick={() => setIsOpen(false)}>resume</Link>
+                    </li>
+                    <li className="mb-3">
+                        <Link onClick={() => setIsOpen(false)}>projects</Link>
+                    </li>
+                    <li className="mb-3">
+                        <Link onClick={() => setIsOpen(false)}>blogs</Link>
+                    </li>
+                    <li className="">
+                        <Link onClick={() => setIsOpen(false)}>contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </Drawer>
+    )
+}
+
+export default MobileNavs
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import ShinyText from "../components/glowingText/ShinyText ";
 import ParticlesBackground from "../components/tsParticles/ParticlesBackground";
 import SocialLinks from "../components/SocialLinks";
-import { Drawer, DrawerHeader, DrawerItems, Button } from "flowbite-react";
+import { HiOutlineBarsArrowUp } from "react-icons/hi2";
+import MobileNavs from "../components/MobileNavs";
+
+
 
 const Home = () => {
 
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const handleDrawerClose = () => setIsDrawerOpen(false);
 
     return (
         <section className="hero-bg">
@@ -20,14 +22,14 @@ const Home = () => {
                     <h5 className="uppercase font-hero md:text-end text-center text-[40px]">
                         <ShinyText text="kaushal varma" speed={3} />
                     </h5>
-                    <p className="text-[#7a7a7a] font-secondary md:text-end text-center text-3xl mb-9">
+                    <div className="text-[#7a7a7a] font-secondary md:text-end text-center text-3xl mb-9">
                         I'm a passionate <span className="uppercase inline-block border-b border-[#27a776] pb-1 text-sm font-hero">
                             <ShinyText text="fullstack developer" speed={3} />
                         </span> from Surat
-                    </p>
-                    <ul className="text-[#7a7a7a] capitalize flex justify-end gap-14 font-semibold nav-link mb-7">
+                    </div>
+                    <ul className="text-[#7a7a7a] capitalize  md:flex hidden justify-end gap-14 font-semibold nav-link mb-7">
                         <li>
-                            <Link>home</Link>
+                            <Link to={"./"}>home</Link>
                         </li>
                         <li>
                             <Link>about</Link>
@@ -45,33 +47,18 @@ const Home = () => {
                             <Link>contact</Link>
                         </li>
                     </ul>
+                    {/* mobile navs toggle button */}
+                    <div className="flex justify-center md:hidden mb-7">
+                        <button className="bg-[#27a776] w-[50px] h-[50px] rounded-full flex items-center justify-center text-white text-2xl" onClick={() => setIsDrawerOpen(!isDrawerOpen)}>
+                            <HiOutlineBarsArrowUp />
+                        </button>
+                    </div>
                     <SocialLinks />
                 </div>
             </div>
-            <div className="md:hidden">
-                <Drawer open={isDrawerOpen} onClose={handleDrawerClose} position="top">
-                    <DrawerHeader title="Navigation" />
-                    <DrawerItems>
-                        <p className="mb-6 text-sm text-gray-500 dark:text-gray-400">
-                            Supercharge your hiring by taking advantage of our&nbsp;
-                            <a href="#" className="text-cyan-600 underline hover:no-underline dark:text-cyan-500">
-                                limited-time sale
-                            </a>
-                            &nbsp;for Flowbite Docs + Job Board.
-                        </p>
-                        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                            <a href="#" className="rounded-lg border bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700">
-                                Learn more
-                            </a>
-                            <a href="#" className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-sm font-medium text-white hover:bg-cyan-800 dark:bg-cyan-600 dark:hover:bg-cyan-700">
-                                Get access&nbsp;
-                                <svg className="ms-2 h-3.5 w-3.5 rtl:rotate-180" viewBox="0 0 14 10" fill="none">
-                                    <path d="M1 5h12m0 0L9 1m4 4L9 9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                </svg>
-                            </a>
-                        </div>
-                    </DrawerItems>
-                </Drawer>
+            {/* mobile navs */}
+            <div className="md:hidden relative z-20">
+                <MobileNavs isOpen={isDrawerOpen} setIsOpen={setIsDrawerOpen} />
             </div>
         </section>
     );
